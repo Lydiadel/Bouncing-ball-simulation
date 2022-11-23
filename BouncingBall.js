d96080150ffeb641aea81ab9ea8b9f87131046e5
Lydiadel <lydia101-lidi@hotmail.com>
2022-11-22 21:38:53 -0600

    Create storyboard summary

--- BouncingBall.js
+++ BouncingBall.js
@@ -22,6 +22,10 @@ function setup() {
                    256, 110,
                    318,115,
                    346,105);
+
+    // Define summary of storyboard only selcted frames
+    storyboard(77, 108, 145, 99,179,125, 205,246);
+    storyboard(214, 276, 256, 110, 318, 115, 346,105);
            
     switch(cases){
       case 1:
@@ -30,6 +34,17 @@ function setup() {
           }
         break
     } 
+  }
+
+  function storyboard(x1, y1, x2, y2, x3, y3, x4, y4) {
+    fill(255);
+    steps = 10;
+    for (i = 0; i <= steps; i++) {
+      t = i / float(steps);
+      x = bezierPoint(x1, x2, x3, x4, t);
+      y = bezierPoint(y1, y2, y3, y4, t);
+     ellipse(x, y, 10, 10);
+    }
   }
 
   function mouseClicked(){
